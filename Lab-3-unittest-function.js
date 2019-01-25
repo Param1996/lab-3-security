31dd87fb6d12c820b5300b81f4abaeb61746d65f
Tarun Kumar <tarun1910.kumar@gmail.com>
2019-01-25 14:29:51 -0500

    updated file

--- Lab-3-unittest-function.js
+++ Lab-3-unittest-function.js
@@ -1,22 +1,14 @@
-//LAB 3 - UNIT TESTING
-//======== THIS FILE IS FOR THE checkHumbrId FUNCTION ==========
-alert("lab 3 Function");//please delete this line once connected.
-/** checkHumbrId Function*/
-function  checkHumberid(studentId)
-{
-	"use strict"
-	let validId=false;
-	var idTester="/^[nN0-9]*/"
-	if idtester.test(studentId){
-		validId= true;
-	}
-	else{
-		validId= false;
-	}
-	return validId;
-}
 
- /* Validate input is a Humber College Student number.
- * Returns true if input validates.
- * @param {string} idIn
- */
+ 
+ 
+function checkHumberId(studentId){
+	let collegeId="false";
+	let pattern=/^(n|N)\d{8}/;
+			if(!pattern.test(studentId)){
+				collegeId= false;
+			}
+			else if(pattern.test(studentId)){
+				collegeId = true;
+			}
+	return collegeId;
+}
--- Lab-3-unittest-test.js
+++ Lab-3-unittest-test.js
@@ -1,4 +1,25 @@
 //LAB 3 - UNIT TESTING
 //======== THIS FILE IS FOR THE UNIT TEST ==========
-alert("lab 3 UNIT TEST");//please delete this line once connected.
 
+function test_checkHumberId (valueIn, expected){
+    
+    
+    // check variable to hold result
+    let result = checkHumberId(valueIn);
+    
+    //print
+    let outMssg = "Input: " +valueIn + "|Result: " +result + "|Expected: "+ expected + "<br/>"
+    
+    let Mssg= document.getElementById("data");
+    Mssg.innerHTML += outMssg;
+    
+    
+}
+
+test_checkHumberId("n01333782", "Pass");
+test_checkHumberId("azjhiosoi;s", "fail");
+test_checkHumberId("jdiloljedliedo","Fail" );
+test_checkHumberId("n012782", "fail");
+test_checkHumberId("n12345654","pass");
+test_checkHumberId("n93939393","pass");
+test_checkHumberId("tarunkumar","fail");
